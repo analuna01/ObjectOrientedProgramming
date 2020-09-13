275786ee5fa1187fa0fd77dd139213900678b2b4
Ana Luna <luna.ana92@gmail.com>
2020-09-12 23:20:25 -0700

    created function to render employees info

--- index.js
+++ index.js
@@ -8,48 +8,45 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-
-// Manager promt Questions
+// Prompt Manager Questions:
 
 const managerQuestions = [
-
     {
         name: "id",
-        message: "Enter manager's ID:"
+        message: "Enter manager's ID:",
     },
 
     {
         name: "name",
-        message: "Enter manager's name:"
+        message: "Enter manager's name:",
     },
 
     {
         name: "email",
-        message: "Enter manager's email:"
+        message: "Enter manager's email:",
     },
 
     {
         name: "officeNumber",
-        message: "Enter your office number:"
+        message: "Enter your office number:",
     },
-
 ];
 
-// Engineer prompt questions:
+// Prompt Engineer Questions:
 const engineerQuestions = [
     {
         name: "id",
-        message: "Enter engineer's ID"
+        message: "Enter engineer's ID",
     },
-    
+
     {
         name: "name",
-        message: "Enter engineer's name:"
+        message: "Enter engineer's name:",
     },
 
     {
         name: "email",
-        message: "Enter engineer's email:"
+        message: "Enter engineer's email:",
     },
 
     {
@@ -58,21 +55,21 @@ const engineerQuestions = [
     },
 ];
 
-// Intern prompt questions:
+// Promt Intern Questions:
 const internQuestions = [
     {
         name: "id",
-        message: "Enter intern's ID"
+        message: "Enter intern's ID",
     },
-    
+
     {
         name: "name",
-        message: "Enter intern's name:"
+        message: "Enter intern's name:",
     },
 
     {
         name: "email",
-        message: "Enter intern's email:"
+        message: "Enter intern's email:",
     },
 
     {
@@ -81,138 +78,151 @@ const internQuestions = [
     },
 ];
 
-// Creates Team
-async function createTeam(managersResponses, engineersResponses, internsResponses) {
+// Team Count:
+const questionCount = [
+    {
+        name: "count",
+        message: "How many do you want to enter?"
+    },
+];
+
+// Creates Team:
+async function createTeam(
+    managersResponses,
+    engineersResponses,
+    internsResponses
+) {
     const managersHTML = [];
     const engineersHTML = [];
     const internsHTML = [];
 
-    managersResponses.forEach(manager => {
-        var managerFile = fs.readFileSync ("./templates/manager.html", "utf8");
+    managersResponses.forEach((manager) => {
+        var managerFile = fs.readFileSync("./templates/manager.html", "utf8");
+
         managerFile = replacePlaceholders(managerFile, "name", manager.getName());
         managerFile = replacePlaceholders(managerFile, "email", manager.getEmail());
         managerFile = replacePlaceholders(managerFile, "role", manager.getRole());
         managerFile = replacePlaceholders(managerFile, "id", manager.getId());
-        managerFile = replacePlaceholders(managerFile, "officeNumber", manager.getofficeNumber());
-        managersHTML.push(managerFile)
+
+        managerFile = replacePlaceholders( managerFile,"officeNumber",manager.getofficeNumber());
+        
+        //Pushes responses to HTML file:
+        managersHTML.push(managerFile);
     });
 
-    engineersResponses.forEach(engineer => {
-        var engineerFile = fs.readFileSync ("./templates/manager.html", "utf8");
-        engineerFile = replacePlaceholders(engineerFile, "name", engineer.getName());
-        engineerFile = replacePlaceholders(engineerFile, "email", engineer.getEmail());
-        engineerFile = replacePlaceholders(engineerFile, "role", engineer.getRole());
+    engineersResponses.forEach((engineer) => {
+        var engineerFile = fs.readFileSync("./templates/engineer.html", "utf8");
+
+        engineerFile = replacePlaceholders( engineerFile, "name", engineer.getName());
+        engineerFile = replacePlaceholders(engineerFile, "email",engineer.getEmail());
+        engineerFile = replacePlaceholders(engineerFile,"role",engineer.getRole());
         engineerFile = replacePlaceholders(engineerFile, "id", engineer.getId());
-        engineerFile = replacePlaceholders(engineerFile, "gitHub", engineer.getGithub());
-        engineersHTML.push(engineerFile)
+
+        engineerFile = replacePlaceholders(engineerFile, "github", engineer.getGithub());
+
+        engineersHTML.push(engineerFile);
     });
 
-    internsResponses.forEach(intern => {
-        var internFile = fs.readFileSync ("./templates/manager.html", "utf8");
+    internsResponses.forEach((intern) => {
+        var internFile = fs.readFileSync("./templates/intern.html", "utf8");
         internFile = replacePlaceholders(internFile, "name", intern.getName());
         internFile = replacePlaceholders(internFile, "email", intern.getEmail());
         internFile = replacePlaceholders(internFile, "role", intern.getRole());
         internFile = replacePlaceholders(internFile, "id", intern.getId());
         internFile = replacePlaceholders(internFile, "school", intern.getSchool());
-        internsHTML.push(internFile)
+        internsHTML.push(internFile);
     });
 
     writeHTMLFile(managersHTML, engineersHTML, internsHTML);
- 
 }
 
-function writeHTMLFile(managersHTML, engineersHTML, internsHTML){
-    var teamFile = fs.readFileSync ("./templates/main.html", "utf8");
+function writeHTMLFile(managersHTML, engineersHTML, internsHTML) {
+    var teamFile = fs.readFileSync("./templates/main.html", "utf8");
     var teamHTML = [];
 
-    teamHTML.push(managersHTML[0]);
-    teamHTML.push(engineersHTML[0]);
-    teamHTML.push(internsHTML[0]);
+    managersHTML.forEach((element) => {
+        teamHTML.push(element);
+    });
+
+    engineersHTML.forEach((element) => {
+        teamHTML.push(element);
+    });
+
+    internsHTML.forEach((element) => {
+        teamHTML.push(element);
+    });
 
     teamHTML = teamHTML.join("");
     teamFile = replacePlaceholders(teamFile, "team", teamHTML);
     writeFileAsync("./output/team.html", teamFile);
-    console.log("Your File Has Been Created!")
+    console.log("********** YOUR TEAM HAS BEEN CREATED! **********");
 }
 
-function replacePlaceholders(template, placeholder, value){
-    const pattern = new RegExp("{{"+ placeholder + "}}", "gm");
+function replacePlaceholders(template, placeholder, value) {
+    const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
     return template.replace(pattern, value);
 }
 
-
-(async function(){
-
-    // Organizes array of questions for manager, engineer and intern
+(async function () {
+    // Organizes array of questions for manager, engineer and intern:
     const managersArray = [];
     const engineersArray = [];
     const internsArray = [];
-    
-    // Promts questions for each employee
-    inquirer
-    .prompt(managerQuestions, async function(response){
-        const newManager = new Manager(response.name, response.id, response.email, response.officeNumber);
-        managersArray.push(newManager);
-    }).then(async function(){
-        await 
-        inquirer
-        .prompt(engineerQuestions, async function(response){
-            const newEngineer = new Engineer(response.name, response.id, response.email, response.gitHub);
-            engineersArray.push(newEngineer);
-        }).then (async function(){
-            await
-            inquirer
-            .prompt(internQuestions, async function(response){
-                const newIntern = new Intern(response.name, response.id, response.email, response.school);
-                internsArray.push(newIntern);
-            });
-        });
-    });
-    
-        
-    })();
-    
- createTeam(managersArray, engineersArray, internsArray);
 
-// inquirer.prompt(questions).then(response =>{
-//     fs.accessSync("./manager.html", (response.name) + {name} , function(error) {
-//         if (error){
-//             return console.log(error);
-//         }
-//         console.log("success!")
-//     });
+    console.log("********** CREATE ENGINEERING TEAM **********");
+   
 
-// })
+    // Manager PROMPT:
+    console.log("====== Manager Info ======");
+    var responseManager = await inquirer.prompt(managerQuestions);
 
+    var newManager = new Manager(
+        responseManager.name,
+        responseManager.id,
+        responseManager.email,
+        responseManager.officeNumber
+    );
+    managersArray.push(newManager);
+    console.log("                            ");
 
+    //Count for Engineers:
+    console.log("====== Engineer Info ======");
+    var responseCount = await inquirer.prompt(questionCount);
+    console.log("                            ");
 
-// const questions = [
-//     {
-//         name: "name",
-//         message: "Enter your full name"
-//     },
-    
-//     {
-//         type: "checkbox",
-//         name: "role",
-//         message: "Select your role",
-//         choices: [
-//             "Manager",
-//             "Engineer",
-//             "Intern",
-//         ]
-//     },
-//     {
-//         name: "email",
-//         message: "Enter your email address"
-//     },
-//     {
-//         name: "id",
-//         message: "Enter your employee ID"
-//     },
-//     {
-//         name: "officeNumber",
-//         message: "Enter your office number"
-//     }
-    
-// ]
+    for (let index = 0; index < parseInt(responseCount.count); index++) {
+        //Engineer PROMPT
+        var responseEngineer = await inquirer.prompt(engineerQuestions);
+        var newEngineer = new Engineer(
+            responseEngineer.name,
+            responseEngineer.id,
+            responseEngineer.email,
+            responseEngineer.gitHub
+        );
+        engineersArray.push(newEngineer);
+        console.log("                            ");
+    }
+    console.log("                            ");
+
+    //Count for Interns
+    console.log("====== Intern Info ======");
+    responseCount = await inquirer.prompt(questionCount);
+    console.log("                            ");
+
+    for (let index = 0; index < parseInt(responseCount.count); index++) {
+        //Intern PROMPT
+        var responseIntern = await inquirer.prompt(internQuestions);
+        var newIntern = new Intern(
+            responseIntern.name,
+            responseIntern.id,
+            responseIntern.email,
+            responseIntern.school
+        );
+        internsArray.push(newIntern);
+        console.log("                            ");
+    }
+    console.log("                            ");
+
+    // Function create team using data captured:
+    await createTeam(managersArray, engineersArray, internsArray);
+})();
